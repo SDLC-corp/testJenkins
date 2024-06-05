618166a9a1eae929cacb6903506475d721d2fffe
gauravsolanke <gaurav@sdlccorp.com>
2024-06-05 12:02:10 +0530

    log env port

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
 require('dotenv').config()
+
+console.log("env : ",process.env.PORT);
+
 const port = process.env.PORT
 
 app.get('/', (req, res) => {
